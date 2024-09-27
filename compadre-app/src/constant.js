7727b495b8dd46c8db769c22f59d133da1c880e3
Lionel Oto <lionel.oto@potloc.com>
2024-09-27 16:11:41 -0400

    Added ranking function to pick the top 3 products

--- compadre-app/src/constant.js
+++ compadre-app/src/constant.js
@@ -3,6 +3,6 @@ import axios from "axios";
 axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*' // for all requests
 
 export const brainUrl = "https://compadre-image-recognition-e711182a825b.herokuapp.com/compadre-image-recognition/generate";
+export const compareUrl = "https://compadre-image-recognition-e711182a825b.herokuapp.com/compadre-image-recognition/compare";
 export const facebookMarketPlaceUrl = "https://214a-2001-18c0-24-9b00-6d15-f928-ebc8-6051.ngrok-free.app/crawl_facebook_marketplace";
-// export const amazonUrl = "https://35e8-2001-18c0-24-9b00-6d15-f928-ebc8-6051.ngrok-free.app/crawl_amazon";
 export const amazonUrl = "https://214a-2001-18c0-24-9b00-6d15-f928-ebc8-6051.ngrok-free.app/crawl_amazon";
--- compadre-app/src/findProducts.js
+++ compadre-app/src/findProducts.js
@@ -1,83 +1,119 @@
 import axios from "axios";
 import priceParser from "price-parser"
 import parsePrice from "parse-price"
-import { amazonUrl, brainUrl, facebookMarketPlaceUrl } from "@/constant.js";
+import {amazonUrl, brainUrl, compareUrl, facebookMarketPlaceUrl} from "@/constant.js";
 
 function formatPrice(price) {
-  const parsedPrice = priceParser.parseFirst(price);
-  const result = Math.max(parsedPrice ? parsedPrice.floatValue : 0, parsePrice(price));
-  return result;
+    const parsedPrice = priceParser.parseFirst(price);
+    return Math.max(parsedPrice ? parsedPrice.floatValue : 0, parsePrice(price));
 }
 
 export const findProducts = {
-  methods: {
-    findProducts: async function(image, cityName, updateDataState) {
-      const results = [];
+    methods: {
+        findProducts: async function (image, cityName, updateDataState) {
+            let results = [];
 
       try {
         const description = await this.getImageDescription(image);
         updateDataState({ descriptionLoaded: true });
 
-        const facebookMarketplaceResults = this.getFacebookMarketplaceProducts(description, cityName);
-        const amazonResults = this.getAmazonProducts(description, cityName);
-        await Promise.all([facebookMarketplaceResults, amazonResults]).then((responses) => {
-          responses.forEach((response) => {
-            if (response.config.url === facebookMarketPlaceUrl) {
-              response.data.forEach((product) => {
-                const result = {
-                  name: product.title,
-                  price: formatPrice(product.price),
-                  currency: "$CAD",
-                  imageUrl: product.image,
-                  url: "facebook.com" + product.link,
-                  source: product.source,
-                };
-                results.push(result);
-              });
-            } else if (response.config.url === amazonUrl) {
-              response.data.forEach((product) => {
-                const result = {
-                  name: product.title,
-                  price: formatPrice(product.price),
-                  currency: "$CAD",
-                  imageUrl: product.image,
-                  url: product.link,
-                  source: product.source,
-                };
-                results.push(result);
-              });
-            } else {
-              console.error("Unknown url")
-              console.error(response)
-            }
-          });
-        });
-        updateDataState({ adsLoaded: true });
-        return results;
-      } catch (error) {
-        updateDataState({ errorOccurred: true })
-        return results;
-      }
-    },
-    getImageDescription: async function(image) {
-      const formData = new FormData();
-      formData.append("file", image);
-      const response = await axios.post(brainUrl, formData, {
-        headers: {
-          "Content-Type": "multipart/form-data"
-        }
-      }).catch();
-      let description = response.data.description;
-      description = description.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
-      description = description.toLowerCase();
+                const facebookMarketplaceResults = this.getFacebookMarketplaceProducts(description, cityName);
+                const amazonResults = this.getAmazonProducts(description, cityName);
+                results = await this.fetchAllProducts(facebookMarketplaceResults, amazonResults);
+                updateDataState({adsLoaded: true});
 
-      return description;
-    },
-    getFacebookMarketplaceProducts: async function(description, cityName) {
-      return axios.get(facebookMarketPlaceUrl, { params: { city: cityName, query: description } });
-    },
-    getAmazonProducts: async function(description) {
-      return axios.get(amazonUrl, { params: { query: description } });
-    },
-  }
+                results = await this.rankProducts(image, results);
+                updateDataState({rankingCompleted: true});
+
+                return results;
+            } catch (error) {
+                updateDataState({errorOccurred: true})
+                return results;
+            }
+        },
+        getImageDescription: async function (image) {
+            const formData = new FormData();
+            formData.append("file", image);
+            const response = await axios.post(brainUrl, formData, {
+                headers: {
+                    "Content-Type" : "multipart/form-data"
+                }
+            }).catch((error) => {
+                throw new Error("Generate query failed")
+            });
+            let description = response.data.description;
+            description = description.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+            description = description.toLowerCase();
+
+            return description;
+        },
+        getFacebookMarketplaceProducts: async function (description, cityName) {
+            return axios.get(facebookMarketPlaceUrl, {
+                params: {
+                    city: cityName,
+                    query: description
+                }
+            }).catch((error) => {
+                throw new Error("Facebook query failed")
+            });
+        },
+        getAmazonProducts: async function (description) {
+            return axios.get(amazonUrl, {params: {query: description}}).catch((error) => {
+                throw new Error("Amazon query failed")
+            });
+        },
+        fetchAllProducts: async function (facebookMarketplaceResults, amazonResults) {
+            const results = [];
+            await Promise.all([facebookMarketplaceResults, amazonResults]).then((responses) => {
+                responses.forEach((response) => {
+                    if (response.config.url === facebookMarketPlaceUrl) {
+                        response.data.forEach((product) => {
+                            const result = {
+                                name: product.title,
+                                price: formatPrice(product.price),
+                                currency: "$CAD",
+                                imageUrl: product.image,
+                                url: "facebook.com" + product.link,
+                                source: product.source,
+                            };
+                            results.push(result);
+                        });
+                    } else if (response.config.url === amazonUrl) {
+                        response.data.forEach((product) => {
+                            const result = {
+                                name: product.title,
+                                price: formatPrice(product.price),
+                                currency: "$CAD",
+                                imageUrl: product.image,
+                                url: product.link,
+                                source: product.source,
+                            };
+                            results.push(result);
+                        });
+                    } else {
+                        console.error("Unknown url")
+                        console.error(response)
+                    }
+                });
+            });
+            return results;
+        },
+        rankProducts: async function (image, products) {
+            const formData = new FormData();
+            formData.append("file", image);
+            formData.append("payload", JSON.stringify({image_list: products}));
+            const response = await axios.post(compareUrl, formData, {
+                headers: {
+                    "Content-Type": "multipart/form-data"
+                }
+            }).catch((error) => {
+                throw new Error("Compare query failed")
+            });
+            response.data.response.sort((a, b) => {
+                return a.MSE - b.MSE;
+            });
+
+            return response.data.response;
+        },
+    }
 }
