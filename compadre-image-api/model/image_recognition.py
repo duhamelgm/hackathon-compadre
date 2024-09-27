7cda936e16336578db05fb5b60d22f836f570925
Jean Philippe Petit-Frere <jeanphilippe.petitfrere@gmail.com>
2024-09-27 00:06:46 -0400

    created the openai api call handler and the Image recog class

--- compadre-image-api/model/image_recognition.py
+++ compadre-image-api/model/image_recognition.py
@@ -1,6 +1,16 @@
+from model.openai_agent import OpenAIAgent
+import base64
+
 class ImageRecognitionAgent:
     def __init__(self) -> None:
-        pass
+        self.openai_agent = OpenAIAgent()
     
-    def predict(self):
-        pass
+    @staticmethod
+    def _encode_image(image_path):
+        with open(image_path, "rb") as image_file:
+            return base64.b64encode(image_file.read()).decode('utf-8')
+        
+    def predict(self, image_path):
+        base64_image = ImageRecognitionAgent._encode_image(image_path)
+        response = self.openai_agent.make_api_call(base64_image)
+        return response
