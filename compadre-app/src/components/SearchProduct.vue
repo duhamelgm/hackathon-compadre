2367958b2242d399b2a8f864899bde70212cbfdf
Vitor Gultzgoff <vitor.gultzgoff@potloc.com>
2024-09-27 00:04:03 -0400

    Include handler for product upload data

--- compadre-app/src/components/SearchProduct.vue
+++ compadre-app/src/components/SearchProduct.vue
@@ -1,13 +1,37 @@
 <template>
   <div class="file is-info is-boxed">
     <label class="file-label">
-      <input class="file-input" type="file" name="resume" />
+      <input class="file-input" type="file" name="product" @change="handleProductUpload" />
       <span class="file-cta">
         <span class="file-icon">
           <i class="fas fa-cloud-upload-alt"></i>
         </span>
         <span class="file-label"> Upload a photo </span>
       </span>
+      <span class="file-name"> No photo uploaded </span>
     </label>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      file: null,
+    };
+  },
+  methods: {
+    handleProductUpload(event) {
+      debugger;
+      this.file = event.target.files[0];
+    }
+  },
+  watch: {
+    file(newFile) {
+      if (newFile) {
+        console.log("File changed: ", newFile.name)
+      }
+    }
+  }
+}
+</script>
