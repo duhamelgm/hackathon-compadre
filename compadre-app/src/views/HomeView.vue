54675f86f11da86baa1ad210354d1e0e79ea3686
Duhamel <duhamelgm@gmail.com>
2024-09-27 16:17:42 -0400

    Adding navbar with logo at the top

--- compadre-app/src/views/HomeView.vue
+++ compadre-app/src/views/HomeView.vue
@@ -4,6 +4,25 @@ import SearchProduct from "../components/SearchProduct.vue"
 
 <template>
   <main>
+    <nav class="navbar navbar-custom" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand">
+        <a class="navbar-item" href="https://bulma.io">
+          COMPADRE.ME
+        </a>
+      </div>
+    </nav>
+
     <SearchProduct />
   </main>
 </template>
+
+<style>
+.navbar-custom {
+  /* background: rgb(250, 68, 56); */
+}
+
+.navbar-custom .navbar-item {
+  font-weight: 700;
+  color: white;
+}
+</style>
