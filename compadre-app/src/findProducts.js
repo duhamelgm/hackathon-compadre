2e21925b9b738039a09db3936b67c54b4b1f7a6e
Lionel Oto <lionel.oto@potloc.com>
2024-09-27 12:24:42 -0400

    Now the image is submitted to the AI to get a description

--- compadre-app/src/findProducts.js
+++ compadre-app/src/findProducts.js
@@ -4,6 +4,7 @@ import parsePrice from "parse-price"
 
 axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*' // for all requests
 
+const brainUrl = "https://3f43-2001-18c0-24-9b00-41bf-46c9-3c85-52a2.ngrok-free.app/compadre-image-recognition/generate";
 const facebookMarketPlaceUrl = "http://192.168.0.187:8000/crawl_facebook_marketplace";
 // const amazonUrl = "https://35e8-2001-18c0-24-9b00-6d15-f928-ebc8-6051.ngrok-free.app/crawl_amazon";
 const amazonUrl = "http://192.168.0.187:8000/crawl_amazon";
@@ -54,12 +55,20 @@ export const findProducts = {
                 });
             });
 
-            console.log(results);
             return results;
         },
         getImageDescription: async function (file) {
-            await new Promise(resolve => setTimeout(resolve, 1000));
-            return "brown sofa comfortable leather used"
+            const formData = new FormData();
+            formData.append("file", file);
+            const response = await axios.post(brainUrl, formData, {
+                headers: {
+                    "Content-Type" : "multipart/form-data"
+                }});
+            let description = response.data.description;
+            description = description.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+            description = description.toLowerCase();
+
+            return description;
         },
         getFacebookMarketplaceProducts: async function (description, cityName) {
             return axios.get(facebookMarketPlaceUrl, {params: {city: cityName, query: description}});
